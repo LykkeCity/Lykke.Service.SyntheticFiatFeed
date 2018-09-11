693457a9246e0c16d9f59184b03c30d74aac9703
Petr Giloian <giloyanp@gmail.com>
2018-09-11 19:22:18 +0700

    LIQ-645 Removed commented code.

--- src/Lykke.Service.SyntheticFiatFeed/Startup.cs
+++ src/Lykke.Service.SyntheticFiatFeed/Startup.cs
@@ -27,38 +27,7 @@ namespace Lykke.Service.SyntheticFiatFeed
                 {
                     logs.AzureTableName = "SyntheticFiatFeedLog";
                     logs.AzureTableConnectionStringResolver = settings => settings.SyntheticFiatFeedService.Db.LogsConnectionString;
-
-                    // TODO: You could add extended logging configuration here:
-                    /* 
-                    logs.Extended = extendedLogs =>
-                    {
-                        // For example, you could add additional slack channel like this:
-                        extendedLogs.AddAdditionalSlackChannel("SyntheticFiatFeed", channelOptions =>
-                        {
-                            channelOptions.MinLogLevel = LogLevel.Information;
-                        });
-                    };
-                    */
                 };
-
-                // TODO: Extend the service configuration
-                /*
-                options.Extend = (sc, settings) =>
-                {
-                    sc
-                        .AddOptions()
-                        .AddAuthentication(MyAuthOptions.AuthenticationScheme)
-                        .AddScheme<MyAuthOptions, KeyAuthHandler>(MyAuthOptions.AuthenticationScheme, null);
-                };
-                */
-
-                // TODO: You could add extended Swagger configuration here:
-                /*
-                options.Swagger = swagger =>
-                {
-                    swagger.IgnoreObsoleteActions();
-                };
-                */
             });
         }
 
@@ -68,19 +37,6 @@ namespace Lykke.Service.SyntheticFiatFeed
             app.UseLykkeConfiguration(options =>
             {
                 options.SwaggerOptions = _swaggerOptions;
-
-                // TODO: Configure additional middleware for eg authentication or maintenancemode checks
-                /*
-                options.WithMiddleware = x =>
-                {
-                    x.UseMaintenanceMode<AppSettings>(settings => new MaintenanceMode
-                    {
-                        Enabled = settings.MaintenanceMode?.Enabled ?? false,
-                        Reason = settings.MaintenanceMode?.Reason
-                    });
-                    x.UseAuthentication();
-                };
-                */
             });
         }
     }
